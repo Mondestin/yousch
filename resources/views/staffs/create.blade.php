4bd8c3f70928aea12a6ed63b6fd12bdaddc3df85
Mondestin Myssie <42750483+Mondestin@users.noreply.github.com>
2022-08-17 13:24:33 +0200

    staff

--- resources/views/staffs/create.blade.php
+++ resources/views/staffs/create.blade.php
@@ -5,12 +5,6 @@
     <div class="my-3 p-3 bg-body rounded shadow-sm">
         <h3 class="border-bottom pb-2 mb-4">Créer un nouveau staff</h3>
         
-        @if (session()->has("success"))
-            <div class="alert alert-success">
-                {{ session()->get('success') }}
-            </div>
-        @endif
-
         <!-- Content Row -->
         <div class="row">
             <!-- Donut Chart -->
